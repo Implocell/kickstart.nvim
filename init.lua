614e87cbe941caf4baa2a7e9ed5404d052a7a54d
Implocell <thomaskgran@gmail.com>
2025-07-21 13:13:20 +0200

    testing

--- init.lua
+++ init.lua
@@ -898,7 +898,6 @@ require('lazy').setup {
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
-    --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'catppuccin/nvim',
     version = false,
