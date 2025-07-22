f0ae3bc91acd58d1353e64f397a9ea9ad1c9f0c5
Implocell <thomaskgran@gmail.com>
2025-07-22 09:45:50 +0200

    makle dotnet great again?

--- lua/custom/plugins/formatting.lua
+++ lua/custom/plugins/formatting.lua
@@ -1,13 +1,24 @@
 return {
   'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format { async = true, lsp_format = 'fallback' }
+      end,
+      mode = '',
+      desc = '[F]ormat buffer',
+    },
+  },
   config = function()
-    local conform = require 'conform'
-
-    conform.setup {
+    local util = require 'conform.util'
+    require('conform').setup {
       formatters = {
         prettier = {
           require_cwd = true,
-          cwd = require('conform.util').root_file {
+          cwd = util.root_file {
             '.prettierrc',
             '.prettierrc.json',
             '.prettierrc.yml',
@@ -24,7 +35,7 @@ return {
         },
         biome = {
           require_cwd = false,
-          cwd = require('conform.util').root_file {
+          cwd = util.root_file {
             'biome.json',
           },
         },
@@ -46,11 +57,22 @@ return {
         html = { 'prettier', 'biome' },
         sql = { 'sleek' },
         json = { 'prettier', 'biome' },
+        cs = { 'csharpier' },
       },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = 'fallback',
-      },
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          return nil
+        else
+          return {
+            timeout_ms = 500,
+            lsp_format = 'fallback',
+          }
+        end
+      end,
     }
   end,
 }
