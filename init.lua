c04f289c5952b24c0f6fee301d0d6c33a234ad93
Implocell <thomaskgran@gmail.com>
2025-07-26 08:04:05 +0200

    formatters, lint and lsp

--- init.lua
+++ init.lua
@@ -592,14 +592,6 @@ require('lazy').setup {
         gopls = {},
         pyright = {},
         rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
         omnisharp = {
           handlers = {
             ['textDocument/definition'] = require('omnisharp_extended').handler,
@@ -644,15 +636,6 @@ require('lazy').setup {
         emmet_ls = {},
         html = {},
         tailwindcss = {},
-        eslint = {
-          on_attach = function(client, bufnr)
-            vim.api.nvim_create_autocmd('BufWritePre', {
-              buffer = bufnr,
-              command = 'EslintFixAll',
-            })
-          end,
-        },
-
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
